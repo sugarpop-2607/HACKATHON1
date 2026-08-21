ccb6ceba7c2641b95d3ac14e436dee074b83d6e6
sugarpop-2607 <jovita260708@gmail.com>
2026-08-21 12:30:10 +0530

    AI implementation

--- ai_imp.py
+++ ai_imp.py
@@ -1,4 +1,5 @@
 from google import genai
+import json
 client = genai.Client(api_key="AQ.Ab8RN6LhvIFytRXfPJ-_If88fTIgZ10eb2SALKtdl2-ucsOCOQ")
 def analyze_quotation(file_path):
     uploaded_file = client.files.upload(file=file_path)
@@ -26,4 +27,15 @@ def analyze_quotation(file_path):
         contents=[uploaded_file, prompt]
     )
 
-    return response.text
+    text = response.text.strip()
+    if text.startswith("```json"):
+        text = text[7:]
+    if text.endswith("```"):
+        text = text[:-3]
+    return json.loads(text.strip()) 
+
+
+
+
+
+
